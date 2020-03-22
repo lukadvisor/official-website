97341b2b203e843dae9f89b1ca0bf717ec8291df
Danny <yuwangleung3@hotmail.com>
2020-03-22 22:24:58 +0800

    Implement Bio
    closes #6

---
+++ js/bio.js
@@ -0,0 +1,176 @@
+const people = {
+    "winson": {
+        "id": "winson",
+        "name": "Winson Luk",
+        "position": "Co-Founder & CEO",
+        "description": "Winson Luk is a data scientist and AI engineer. He is a computer science graduate from Hong Kong University of Science and Technology. He won over 10 awards from national hackathons, programming and data science competitions before. With the working experience in UBS, Uber, Huawei, Fidelity and Boston Consulting, he is sure he can provide valuable solutions to any client.",
+        "image": "winson.jpg",
+        "links": [
+            {
+                "type": "github",
+                "icon": "fa-github",
+                "link": "https://github.com/wslukaa"
+            },
+            {
+                "type": "linkedin",
+                "icon": "fa-linkedin-in",
+                "link": "https://www.linkedin.com/in/wing-san-luk-517766ab/"
+            }
+        ]
+    },
+    "shaun": {
+        "id": "shaun",
+        "name": "Shaun Tsoi",
+        "position": "Co-Founder & Principal Project Manager",
+        "description": "Shaun Tsoi is an innovative and passionate Studentpreneur in Hong Kong, with over 20 competition achievements, awards and scholarships in innovation. He has solid experience in startups’ business development, such as ongoing startups providing smoking cessation IoT, data-driven health solution and food design, and achieved over million funding. He also have business development experience in HP and Ocean Network Express.\n\nHe is also a community service enthusiast. He cofounded an NGO(ongoing) facilitating youth development, and was the top management of an entrepreneurship organization fostering innovative spirit among students.\n\nOne more fun fact, he designed and owned a virtual influencer(ongoing) on social media with over thousand fans in 1 month, which demonstrated his talent in marketing and design.",
+        "image": "shaun.jpg",
+        "links": [
+            {
+                "type": "linkedin",
+                "icon": "fa-linkedin-in",
+                "link": "https://www.linkedin.com/in/shauntsoi/"
+            }
+        ]
+    },
+    "patrick": {
+        "id": "patrick",
+        "name": "Patrick Lam",
+        "position": "Co-Founder & Principal Strategy Consultant",
+        "description": "Patrick Lam is a strategist specialized in digital transformation. He has diversified experience in conglomerates like Swire, financial institutions and FinTech unicorn Alipay. Pursuing his bachelor’s degree in Risk Management and Business Intelligence at HKUST, he is also working with Google to formulate new digital strategies for the insurance sector.",
+        "image": "patrick.jpg",
+        "links": [
+            {
+                "type": "linkedin",
+                "icon": "fa-linkedin-in",
+                "link": "https://www.linkedin.com/in/patrick-lam-a8a925139"
+            }
+        ]
+    },
+    "samson": {
+        "id": "samson",
+        "name": "Samson Lam",
+        "position": "Co-Founder & Principal Technical Consultant",
+        "description": "Samson is a HKU Computer Science student who is eager to gain a more detailed understanding of Technology. Believing in joining various programmes and competitions, he can have a chance to pursue the advanced technology. He will surely unravel the knowledge diligently and make contributions to the discipline for the betterment of Technology.",
+        "image": "samson.jpg",
+        "links": [
+            {
+                "type": "github",
+                "icon": "fa-github",
+                "link": "https://github.com/samsonllam"
+            },
+            {
+                "type": "linkedin",
+                "icon": "fa-linkedin-in",
+                "link": "https://www.linkedin.com/in/samson-lam/"
+            }
+        ]
+    },
+    "martin": {
+        "id": "martin",
+        "name": "Martin Ng",
+        "position": "Data Consultant",
+        "description": "Martin Ng is a Data Analyst. Graduated from HKUST in Statistics and Financial Mathematics, Martin Ng is currently pursuing a Master's degree in Mathematics at CUHK. He worked in HSBC and Big4 accounting firms, and provided assistance in many data related projects.",
+        "image": "martin.jpg",
+        "links": [
+            {
+                "type": "linkedin",
+                "icon": "fa-linkedin-in",
+                "link": "https://www.linkedin.com/in/martin-ng-54b2b5131"
+            }
+        ]
+    },
+    "dennis": {
+        "id": "dennis",
+        "name": "Dennis Tsang",
+        "position": "Developer",
+        "description": "Dennis Tsang is a full stack developer interested in how digital innovations shape a better future. Graduated from HKUST, he worked at fast growing e-commerce and fintech companies for projects from user-friendly apps to tech-focus complex systems.",
+        "image": "dennis.jpg",
+        "links": [
+            {
+                "type": "github",
+                "icon": "fa-github",
+                "link": "https://github.com/cstsangac"
+            }
+        ]
+    },
+    "fishball": {
+        "id": "fishball",
+        "name": "Fishball Yu",
+        "position": "Developer",
+        "description": "Fishball Yu is a full-stack web developer. He is a graduate from the Hong Kong University of Science and Technology, majoring in Computer Science with a first class honor. As a fun-loving person, he is interested in developing applications that ease daily chores and benefit the lives of people.",
+        "image": "fishball.jpg",
+        "links": [
+            {
+                "type": "github",
+                "icon": "fa-github",
+                "link": "https://github.com/FishballDannyYWL"
+            }
+        ]
+    },
+    "eric": {
+        "id": "eric",
+        "name": "Eric So",
+        "position": "Project Manager & Data Analyst",
+        "description": "Eric So is a project manager and data analyst. He graduated from City University of Hong Kong with bachelor’s degree in Information Management. He worked in a fast-growing data company and assisted in various data projects.",
+        "image": "eric.jpg",
+        "links": []
+    },
+    "peter": {
+        "id": "peter",
+        "name": "Peter Lee",
+        "position": "Researcher",
+        "description": "Peter Lee is currently finishing his PhD in Computer Science in US. His research interest is using artificial intelligence in medical imaging perspectives. Before that, he completed a master degree in Columbia University and a bachelor degree in The Chinese University of Hong Kong. He has several publications in well-known conferences and journals in medical domain with the use of deep learning.",
+        "image": "peter.jpg",
+        "links": [
+            {
+                "type": "linkedin",
+                "icon": "fa-linkedin-in",
+                "link": "https://www.linkedin.com/in/hl3003/"
+            },
+            {
+                "type": "github",
+                "icon": "fa-github",
+                "link": "https://github.com/leeh43/"
+            }
+        ]
+    },
+    "steve": {
+        "id": "steve",
+        "name": "Steve Chak",
+        "position": "Researcher",
+        "description": "Steve Chak is currently a PhD student in the Graduate Group of Applied Mathematics. His research interests are optimisation, machine learning and scientific computation. He had a MPhil degree and a BSc degree (first class honours) in Mathematics in CUHK. He is experienced in deep learning research and medical imaging. He cooperated with Two Sigma previously for quantitative research in finance",
+        "image": "steve.jpg",
+        "links": [
+            {
+                "type": "linkedin",
+                "icon": "fa-linkedin-in",
+                "link": "https://www.linkedin.com/in/stevechak"
+            }
+        ]
+    },
+    "jacky": {
+        "id": "jacky",
+        "name": "Jacky Lo",
+        "position": "Advisor",
+        "description": "Jacky Lo is a Business advisor. He had a double degree in Computer Engineering and Commerce. He loves to connect tech to businesses to save people time for leisure and outdoor activities. He worked in both the Big4 accounting and German sourcing companies for business development and solution deployment.",
+        "image": "jacky.jpg",
+        "links": [
+            {
+                "type": "linkedin",
+                "icon": "fa-linkedin-in",
+                "link": "https://www.linkedin.com/in/jacky-lo-462211a2"
+            }
+        ]
+    }
+};
+
+function displayBio(person) {
+    const data = people[person];
+
+    $('#bio-name').text(data.name);
+    $('#bio-position').text(data.position);
+    $('#bio-description').text(data.description);
+    $('#bio-image').attr('src', `images/teams/${data.image}`);
+
+    $('#bio').modal();
+}
